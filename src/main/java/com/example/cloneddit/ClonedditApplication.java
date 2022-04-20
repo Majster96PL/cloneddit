9d4b1d5cb78f2f23f4504087790b5bc6eaa0d23e
Majster96PL <piotruniok96@gmail.com>
2022-04-20 21:26:42 +0200

    Initial commit

---
+++ src/main/java/com/example/cloneddit/ClonedditApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloneddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClonedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClonedditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloneddit/ClonedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloneddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClonedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
