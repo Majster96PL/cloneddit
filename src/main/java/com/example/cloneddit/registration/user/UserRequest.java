5f0fa25cdb9079c41e756c3940f364b7131a556d
Majster96PL <piotruniok96@gmail.com>
2022-04-27 16:25:01 +0200

    Create UserRequest necessary to registration

---
+++ src/main/java/com/example/cloneddit/registration/user/UserRequest.java
@@ -0,0 +1,14 @@
+package com.example.cloneddit.registration.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequest {
+    private String userName;
+    private String email;
+    private String password;
+}
