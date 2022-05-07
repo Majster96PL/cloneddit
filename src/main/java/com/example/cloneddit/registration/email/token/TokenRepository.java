f8faa4ba8e9122bd954eebee7ddf6f5ab72ec6f8
Majster96PL <piotruniok96@gmail.com>
2022-05-07 11:38:03 +0200

    Create token service and add method update token to repository

--- src/main/java/com/example/cloneddit/registration/email/token/TokenRepository.java
+++ src/main/java/com/example/cloneddit/registration/email/token/TokenRepository.java
@@ -1,11 +1,20 @@
 package com.example.cloneddit.registration.email.token;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
 public interface TokenRepository extends JpaRepository<Token, Long> {
     Optional<Token> findByToken(String token);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Token t SET t.confirmedDate = ?2 WHERE t.token = ?1")
+    int updateToken(String token, LocalDateTime confirmedDate);
 }
---
+++ src/main/java/com/example/cloneddit/registration/email/token/TokenService.java
@@ -0,0 +1,26 @@
+package com.example.cloneddit.registration.email.token;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class TokenService {
+
+    private final TokenRepository tokenRepository;
+
+    public void saveToken(Token token){
+        tokenRepository.save(token);
+    }
+
+    public Optional<Token> getToken(String token){
+        return tokenRepository.findByToken(token);
+    }
+
+    public int setConfirmedToken(String token){
+        return tokenRepository.updateToken(token, LocalDateTime.now());
+    }
+}
