beffcf312032e01cbf27ec780f55edd08660fdbe
Majster96PL <piotruniok96@gmail.com>
2022-06-10 22:10:10 +0200

    Create Mapper by mapstruct and generated implementation Class

---
+++ src/main/java/com/example/cloneddit/clone/mapper/ClonedditMapper.java
@@ -0,0 +1,20 @@
+package com.example.cloneddit.clone.mapper;
+
+import com.example.cloneddit.clone.Cloneddit;
+import com.example.cloneddit.clone.ClonedditDTO;
+import com.example.cloneddit.clone.post.Post;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ClonedditMapper {
+
+    @Mapping(target = "postCount", expression = "java(postsMap(cloneddit.getPosts()))")
+    ClonedditDTO mapCloneToDTO(Cloneddit cloneddit);
+
+    default Integer postsMap(List<Post> posts){
+        return posts.size();
+    }
+}
