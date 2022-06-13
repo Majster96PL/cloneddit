5d84f9428d9e5980c5d0258b8e6a475010252194
Majster96PL <piotruniok96@gmail.com>
2022-06-13 15:45:30 +0200

    Update login response responsible for login users

--- src/main/java/com/example/cloneddit/api/login/LoginResponse.java
+++ src/main/java/com/example/cloneddit/api/login/LoginResponse.java
@@ -1,11 +1,17 @@
 package com.example.cloneddit.api.login;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+
+import java.time.Instant;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class LoginResponse {
     private String authenticationToken;
+    private String refreshToken;
+    private Instant expire;
     private String username;
 }
