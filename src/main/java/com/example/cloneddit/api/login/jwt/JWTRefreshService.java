6dde8601abd9d87d07d7a58cda773e19c8d8c541
Majster96PL <piotruniok96@gmail.com>
2022-06-13 15:47:10 +0200

    Create service responsible refresh token

---
+++ src/main/java/com/example/cloneddit/api/login/jwt/JWTRefreshService.java
@@ -0,0 +1,32 @@
+package com.example.cloneddit.api.login.jwt;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+@Transactional
+public class JWTRefreshService {
+
+    private final JWTRefreshRepository jwtRefreshRepository;
+
+    public JWTRefresh generateToken(){
+        JWTRefresh refreshToken = new JWTRefresh();
+        refreshToken.setToken(UUID.randomUUID().toString());
+        refreshToken.setCreated(Instant.now());
+        return jwtRefreshRepository.save(refreshToken);
+    }
+
+    public void validateToken(String token){
+        jwtRefreshRepository.findByToken(token)
+                .orElseThrow(() -> new RuntimeException("Invalid Token!"));
+    }
+
+    private void deleteToken(String token){
+        jwtRefreshRepository.deleteByToken(token);
+    }
+}
